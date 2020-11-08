472a28b6342adce406c9662e261a6253a44d7f11
Felix Lindau <a11948883@unet.univie.ac.at>
2020-11-08 21:41:52 +0100

    updated UI, FloatingActionButtons

--- implementation/at/ac/univie/view/app/src/main/java/com/se2ws2020team0310/sketchapp/MainActivity.java
+++ implementation/at/ac/univie/view/app/src/main/java/com/se2ws2020team0310/sketchapp/MainActivity.java
@@ -1,10 +1,16 @@
 package com.se2ws2020team0310.sketchapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+
+import com.google.android.material.appbar.AppBarLayout;
+
+import static com.se2ws2020team0310.sketchapp.R.id.appbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
