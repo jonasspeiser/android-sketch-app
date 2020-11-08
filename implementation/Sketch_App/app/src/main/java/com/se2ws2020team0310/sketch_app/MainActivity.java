5e2a1f58347357eea4cbc5e2a78d5583edc8cb73
Felix Lindau <a11948883@unet.univie.ac.at>
2020-11-09 00:14:06 +0100

    Updated UI, FloatingActionButtons

---
+++ implementation/Sketch_App/app/src/main/java/com/se2ws2020team0310/sketch_app/MainActivity.java
@@ -0,0 +1,29 @@
+package com.se2ws2020team0310.sketch_app;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.se2ws2020team0310.sketch_app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.clear();
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+}
