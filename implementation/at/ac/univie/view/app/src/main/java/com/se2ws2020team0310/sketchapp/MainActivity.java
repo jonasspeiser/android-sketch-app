cfb932942bab0eb83ce681c27fd69ffbb06f6e7a
Felix Lindau <a11948883@unet.univie.ac.at>
2020-11-08 14:48:17 +0100

    initial commit, working version

---
+++ implementation/at/ac/univie/view/app/src/main/java/com/se2ws2020team0310/sketchapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.se2ws2020team0310.sketchapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.clear();
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+}
