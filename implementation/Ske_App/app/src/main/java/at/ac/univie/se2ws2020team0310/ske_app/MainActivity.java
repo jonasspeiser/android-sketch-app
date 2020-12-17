19bac16571f092c4a8a7123ff19df4b2b729647d
Felix Lindau <a11948883@unet.univie.ac.at>
2020-12-17 13:30:52 +0100

    Data Binding implementation 2

--- implementation/Ske_App/app/src/main/java/at/ac/univie/se2ws2020team0310/ske_app/MainActivity.java
+++ implementation/Ske_App/app/src/main/java/at/ac/univie/se2ws2020team0310/ske_app/MainActivity.java
@@ -1,14 +1,20 @@
 package at.ac.univie.se2ws2020team0310.ske_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+
+import at.ac.univie.se2ws2020team0310.ske_app.databinding.ActivityMainBinding;
+import at.ac.univie.se2ws2020team0310.ske_app.viewmodel.AppViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        activityMainBinding.setViewModel(new AppViewModel());
+        activityMainBinding.executePendingBindings();
     }
 }
---
+++ implementation/Ske_App/app/src/main/java/at/ac/univie/se2ws2020team0310/ske_app/viewmodel/AppViewModel.java
@@ -0,0 +1,6 @@
+package at.ac.univie.se2ws2020team0310.ske_app.viewmodel;
+
+import androidx.databinding.BaseObservable;
+
+public class AppViewModel extends BaseObservable {
+}
