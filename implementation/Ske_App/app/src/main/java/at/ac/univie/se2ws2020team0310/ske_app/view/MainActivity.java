6d9fafc8253ad6e366d8e257afa825d0ebff2be0
Felix Lindau <a11948883@unet.univie.ac.at>
2020-12-17 16:23:14 +0100

    Implement Listeners for SeekBarValues

--- implementation/Ske_App/app/src/main/java/at/ac/univie/se2ws2020team0310/ske_app/view/MainActivity.java
+++ implementation/Ske_App/app/src/main/java/at/ac/univie/se2ws2020team0310/ske_app/view/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 import at.ac.univie.se2ws2020team0310.ske_app.R;
 import at.ac.univie.se2ws2020team0310.ske_app.databinding.ActivityMainBinding;
@@ -11,11 +12,18 @@ import at.ac.univie.se2ws2020team0310.ske_app.viewmodel.AppViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SeekBar strokeWidthSeekBar;
+    private SeekBar sizeSeekBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         activityMainBinding.setViewModel(new AppViewModel());
         activityMainBinding.executePendingBindings();
+
+        strokeWidthSeekBar = findViewById(R.id.strokeWidthSeekBar);
+        sizeSeekBar = findViewById(R.id.sizeSeekBar);
+
     }
 }
--- implementation/Ske_App/app/src/main/java/at/ac/univie/se2ws2020team0310/ske_app/viewmodel/AppViewModel.java
+++ implementation/Ske_App/app/src/main/java/at/ac/univie/se2ws2020team0310/ske_app/viewmodel/AppViewModel.java
@@ -1,6 +1,41 @@
 package at.ac.univie.se2ws2020team0310.ske_app.viewmodel;
 
+import android.widget.SeekBar;
+
 import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+import at.ac.univie.se2ws2020team0310.ske_app.BR;
+import at.ac.univie.se2ws2020team0310.ske_app.R;
 
 public class AppViewModel extends BaseObservable {
+
+    int strokeWidthSeekBarValue;
+    int sizeSeekBarValue;
+    boolean strokeWidthSeekBarVisibility;
+    boolean sizeSeekBarVisibility;
+
+    @Bindable
+    public int getSizeSeekBarValue(){
+        return sizeSeekBarValue;
+    }
+
+    //BR class is auto-generated, holds the specific resource for the data binding
+    public void setSizeSeekBarValue(int progress){
+        sizeSeekBarValue = progress;
+        notifyPropertyChanged(BR.sizeSeekBarValue);
+        setSizeSeekBarValue(progress);
+    }
+
+    @Bindable
+    public int getStrokeWidthSeekBarValue(){
+        return strokeWidthSeekBarValue;
+    }
+
+    public void setStrokeWidthSeekBarValue(int progress){
+        strokeWidthSeekBarValue = progress;
+        notifyPropertyChanged(BR.strokeWidthSeekBarValue);
+        setStrokeWidthSeekBarValue(progress);
+    }
+
 }
