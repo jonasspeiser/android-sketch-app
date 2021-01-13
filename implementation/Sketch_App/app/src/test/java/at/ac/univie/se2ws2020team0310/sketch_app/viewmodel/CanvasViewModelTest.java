3db320657c7af36ba3d54b272419c21844bc55b6
Felix Lindau <a11948883@unet.univie.ac.at>
2021-01-13 17:38:53 +0100

    Added Unit test for CanvasViewModel

--- implementation/Sketch_App/app/src/test/java/at/ac/univie/se2ws2020team0310/sketch_app/viewmodel/CanvasViewModelTest.java
+++ implementation/Sketch_App/app/src/test/java/at/ac/univie/se2ws2020team0310/sketch_app/viewmodel/CanvasViewModelTest.java
@@ -2,6 +2,41 @@ package at.ac.univie.se2ws2020team0310.sketch_app.viewmodel;
 
 import junit.framework.TestCase;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+
+import at.ac.univie.se2ws2020team0310.sketch_app.model.Sketch;
+import at.ac.univie.se2ws2020team0310.sketch_app.model.observerPatterInterfaces.CustomObserver;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CanvasViewModelTest extends TestCase {
+
+    @Mock
+    ArrayList<CustomObserver> observers;
+    @Mock
+    CustomObserver observer;
+    @Mock
+    CanvasViewModel canvasViewModel;
+    @Mock
+    Sketch sketch;
+
+    //Test on functionality of observer adding to list of observers
+    @Test
+    public void testRegisterObserver() {
+        canvasViewModel.registerObserver(observer);
+        assertFalse(observers.isEmpty());
+    }
+
+//    //Test on Coordinate change function
+//    @Test
+//    public void testElementCoordinateChange() {
+//        canvasViewModel.setElementCoordinates((float) 0.43,(float) 0.5);
+//        canvasViewModel.changeElementCoordinates((float) 0.44,(float) 0.4,(float) 0.5,(float) 0.6);
+//        assertEquals(sketch)
+//    }
 
 }
